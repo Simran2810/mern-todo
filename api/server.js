f76971c021dc77810d42d86577c0607d7b76d4f8
Simran Nayak <simrannayak427@gmail.com>
2022-03-10 14:20:51 +0530

    added mongo db conn string

--- api/server.js
+++ api/server.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/react-todo', {
+require("dotenv").config()
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/react-todo', {
 	useNewUrlParser: true, 
 	useUnifiedTopology: true 
 }).then(() => console.log("Connected to MongoDB")).catch(console.error);
